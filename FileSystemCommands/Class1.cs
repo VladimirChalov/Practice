d140773ae2fd489497601d457fb05667096a7cc8
VladimirChalov <vladimircalov557@gmail.com>
2025-07-01 18:54:25 +0600

    fix

--- FileSystemCommands/Class1.cs
+++ FileSystemCommands/Class1.cs
@@ -1,53 +1,61 @@
 using CommandLib;
 using System.IO;
 
-namespace FileSystemCommands
+namespace FileSystemCommands;
+
+public class DirectorySizeCommand : ICommand
 {
-    public class DirectorySizeCommand : ICommand
+    public string DirectoryPath {get;}
+    public long Size {get; set;}
+
+    public DirectorySizeCommand(string input)
     {
-        public string DirectoryPath {get;}
-        public int Size {get; set;}
-
-        public DirectorySizeCommand(string input)
-        {
-            DirectoryPath = input;
-            Size = 0;
-        }
-
-        public void Execute()
-        {
-            Size = 0;
-            if (Directory.Exists(DirectoryPath))
-            {
-                foreach (var file in Directory.GetFiles(DirectoryPath, "*", SearchOption.AllDirectories))
-                {
-                    Size += new FileInfo(file).Length;
-                }
-            }
-        }
+        DirectoryPath = input;
+        Size = 0;
     }
-    public class FindFilesCommand : ICommand
-    {
-        public string DirectoryPath {get;}
-        public string SearchPattern {get;}
-        public string[] FoundFiles {get; set;}
 
-        public FindFilesCommand(string input_directory_path, string input_search_pattern)
-        {
-            DirectoryPath = input_directory_path;
-            SearchPattern = input_search_pattern;
-            FoundFiles = Array.Empty<string>();
-        }
     public void Execute()
-{
-    if (Directory.Exists(DirectoryPath))
     {
-        FoundFiles = Directory.GetFiles(DirectoryPath, SearchPattern, SearchOption.AllDirectories);
+        Size = CalculateDirectorySize(DirectoryPath);
+        Console.WriteLine($"Directory size: {Size} bytes");
     }
-    else
+
+    private long CalculateDirectorySize(string path)
     {
+        if (!Directory.Exists(path))
+            throw new DirectoryNotFoundException(path);
+
+        return Directory.GetFiles(path, "*", SearchOption.AllDirectories)
+                       .Sum(file => new FileInfo(file).Length);
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    public string DirectoryPath {get;}
+    public string SearchPattern {get;}
+    public string[] FoundFiles {get; private set;}
+
+    public FindFilesCommand(string directoryPath, string searchPattern)
+    {
+        DirectoryPath = directoryPath;
+        SearchPattern = searchPattern;
         FoundFiles = Array.Empty<string>();
     }
-}
-}
+
+    public void Execute()
+    {
+        if (Directory.Exists(DirectoryPath))
+        {
+            FoundFiles = Directory.GetFiles(DirectoryPath, SearchPattern, SearchOption.AllDirectories);
+            Console.WriteLine($"Found {FoundFiles.Length} files:");
+            foreach (var file in FoundFiles)
+                Console.WriteLine(file);
+        }
+        else
+        {
+            Console.WriteLine("Directory not found");
+            FoundFiles = Array.Empty<string>();
+        }
+    }
 }
