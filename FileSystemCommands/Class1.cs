fbe2b8d97d6a7a0244f46002cc3b8f141db14e38
VladimirChalov <vladimircalov557@gmail.com>
2025-07-01 18:24:22 +0600

    Update Class1.cs

--- FileSystemCommands/Class1.cs
+++ FileSystemCommands/Class1.cs
@@ -1,6 +1,53 @@
-﻿namespace FileSystemCommands;
+using CommandLib;
+using System.IO;
 
-public class Class1
+namespace FileSystemCommands
 {
+    public class DirectorySizeCommand : ICommand
+    {
+        public string DirectoryPath {get;}
+        public int Size {get; set;}
 
+        public DirectorySizeCommand(string input)
+        {
+            DirectoryPath = input;
+            Size = 0;
+        }
+
+        public void Execute()
+        {
+            Size = 0;
+            if (Directory.Exists(DirectoryPath))
+            {
+                foreach (var file in Directory.GetFiles(DirectoryPath, "*", SearchOption.AllDirectories))
+                {
+                    Size += new FileInfo(file).Length;
+                }
+            }
+        }
+    }
+    public class FindFilesCommand : ICommand
+    {
+        public string DirectoryPath {get;}
+        public string SearchPattern {get;}
+        public string[] FoundFiles {get; set;}
+
+        public FindFilesCommand(string input_directory_path, string input_search_pattern)
+        {
+            DirectoryPath = input_directory_path;
+            SearchPattern = input_search_pattern;
+            FoundFiles = Array.Empty<string>();
+        }
+    public void Execute()
+{
+    if (Directory.Exists(DirectoryPath))
+    {
+        FoundFiles = Directory.GetFiles(DirectoryPath, SearchPattern, SearchOption.AllDirectories);
+    }
+    else
+    {
+        FoundFiles = Array.Empty<string>();
+    }
+}
+}
 }
