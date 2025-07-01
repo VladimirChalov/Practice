c0582cbb9826e4895c51859234a1b69d20319d20
VladimirChalov <vladimircalov557@gmail.com>
2025-07-01 18:41:15 +0600

    Update Program.cs

--- CommandRunner/Program.cs
+++ CommandRunner/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Reflection;
+﻿using FileSystemCommands;
+
+class task08
+{
+    static void Main()
+    {
+        string testFolder = @"C:\Users\vladi\OneDrive\Рабочий стол\ТЕСТ";
+        string searchPattern = "*.*";
+
+        var dll = Assembly.LoadFrom("FileSystemCommands.dll");
+
+        var sizeCmd = (dynamic)dll.CreateInstance("FileSystemCommands.DirectorySizeCommand");
+        sizeCmd.Execute(testFolder);
+        Console.WriteLine($"Размер папки: {sizeCmd.Size}");
+
+        var findCmd = (dynamic)dll.CreateInstance("FileSystemCommands.FindFilesCommand");
+        findCmd.Execute(testFolder, searchPattern);
+        
+        Console.WriteLine("\nНайденные файлы:");
+        foreach (var file in findCmd.FoundFiles)
+            Console.WriteLine(Path.GetFileName(file));
+    }
+}
