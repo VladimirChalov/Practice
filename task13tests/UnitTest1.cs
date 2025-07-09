01c8126c39cc2e9b573f4d3b3fdb968f48ea4452
VladimirChalov <vladimircalov557@gmail.com>
2025-07-09 14:09:11 +0600

    Update UnitTest1.cs

--- task13tests/UnitTest1.cs
+++ task13tests/UnitTest1.cs
@@ -1,10 +1,35 @@
+using task13;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Xunit;
+
 namespace task13tests;
 
-public class UnitTest1
+class task13tests{
+    
+[Fact]
+public void Serialize_Student_Works()
 {
-    [Fact]
-    public void Test1()
+    var student = new Student
     {
+        FirstName = "Анна",
+        Grades = new List<Subject> { new Subject { Name = "Литература" } }
+    };
 
-    }
+    string json = JsonSerializer.Serialize(student);
+    
+    if (string.IsNullOrEmpty(json)) 
+        throw new Exception("Не работае");
+}
+
+[Fact]
+public void Deserialize_Student_Works()
+{
+    string json = @"{""FirstName"":""Иван""}";
+    var student = JsonSerializer.Deserialize<Student>(json);
+    
+    if (student?.FirstName != "Иван")
+        throw new Exception("Не работае");
+}
 }
