e19d79a2d647c7aa54ee0ad45e1057ec7c0c91b2
VladimirChalov <vladimircalov557@gmail.com>
2025-07-09 14:05:31 +0600

    Update Class1.cs

--- task13/Class1.cs
+++ task13/Class1.cs
@@ -1,6 +1,37 @@
-﻿namespace task13;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
-public class Class1
+class Subject
 {
+  public string Name {get; set; }
+  public int Grade {get; set; }
+}
 
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public List<Subject> Grades { get; set; }
+}
+
+public class Convert : JsonConverter<DateTime>
+{
+   string model = "yyyy-MM-dd";
+
+   public override DateTime Read(string Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+{
+    string dateString = reader.GetString();
+    DateTime result = DateTime.ParseExact(dateString, model, null);
+    return result;
+}
+
+  public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+{
+    string dateString = value.ToString("yyyy-MM-dd");
+    writer.WriteStringValue(dateString);
+}
 }
