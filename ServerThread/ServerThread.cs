2ab27116c4c7c81e0863ebf2629e1d0b0115c151
VladimirChalov <vladimircalov557@gmail.com>
2025-07-16 16:32:23 +0600

    fix

--- ServerThread/ServerThread.cs
+++ ServerThread/ServerThread.cs
@@ -1,80 +1,81 @@
 using System;
 using System.Collections.Concurrent;
 using System.Threading;
-﻿using Command;
+using Command;
 
-namespace task17{
-public class ServerThread
+namespace task17
 {
-    private readonly ConcurrentQueue<ICommand> q = new();
-    private bool stopNow;
-    private bool stopAfter;
-    public bool running = true;
-    public int id;
-
-    public void Start()
+    public class ServerThread
     {
-        var t = new Thread(Run);
-        t.Start();
-        id = t.ManagedThreadId;
-    }
+        public readonly ConcurrentQueue<ICommand> q = new();
+        public bool stopNow;
+        public bool stopAfter;
+        public bool running = true;
+        public int id;
 
-    public void Add(ICommand cmd)
-    {
-        if (cmd is HardStop)
+        public void Start()
         {
-            while (q.TryDequeue(out _)) { }
+            var t = new Thread(Run);
+            t.Start();
+            id = t.ManagedThreadId;
         }
-        q.Enqueue(cmd);
-    }
 
-    private void Run()
-    {
-        while (!stopNow)
+        public void Add(ICommand cmd)
         {
-            if (q.TryDequeue(out var cmd))
+            if (cmd is HardStop)
             {
-                cmd.Execute();
-                continue;
+                while (q.TryDequeue(out _)) { }
             }
-
-            if (stopAfter)
-            {
-                running = false;
-                return;
-            }
-
-            Thread.Sleep(100);
+            q.Enqueue(cmd);
         }
-        running = false;
+
+        private void Run()
+        {
+            while (!stopNow)
+            {
+                if (q.TryDequeue(out var cmd))
+                {
+                    cmd.Execute();
+                    continue;
+                }
+
+                if (stopAfter)
+                {
+                    running = false;
+                    return;
+                }
+
+                Thread.Sleep(100);
+            }
+            running = false;
+        }
     }
-}
 
-public class HardStop : ICommand
-{
-    private readonly ServerThread t;
-
-    public HardStop(ServerThread t) => this.t = t;
-
-    public void Execute()
+    public class HardStop : ICommand
     {
-        if (Thread.CurrentThread.ManagedThreadId != t.id)
-            throw new InvalidOperationException();
-        t.stopNow = true;
+        private readonly ServerThread t;
+
+        public HardStop(ServerThread t) => this.t = t;
+
+        public void Execute()
+        {
+            if (Thread.CurrentThread.ManagedThreadId != t.id)
+                throw new InvalidOperationException();
+            t.stopNow = true;
+        }
     }
-}
 
-public class SoftStop : ICommand
-{
-    private readonly ServerThread t;
-
-    public SoftStop(ServerThread t) => this.t = t;
-
-    public void Execute()
+    public class SoftStop : ICommand
     {
-        if (Thread.CurrentThread.ManagedThreadId != t.id)
-            throw new InvalidOperationException();
-        t.stopAfter = true;
+        private readonly ServerThread t;
+
+        public SoftStop(ServerThread t) => this.t = t;
+
+        public void Execute()
+        {
+            if (Thread.CurrentThread.ManagedThreadId != t.id)
+                throw new InvalidOperationException();
+            t.stopAfter = true;
+        }
     }
-}
 }
