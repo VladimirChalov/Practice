06c786f2894f60bb6c5552731ccc91b37203da43
VladimirChalov <vladimircalov557@gmail.com>
2025-07-17 19:41:33 +0600

    Update Class1.cs

--- task05/Class1.cs
+++ task05/Class1.cs
@@ -1,6 +1,80 @@
-﻿namespace task05;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 
-public class Class1
+namespace task05
 {
+  
+public class ClassAnalyzer
+{
+    private Type type;
 
+    public ClassAnalyzer(Type t)
+    {
+        type = t;
+    }
+
+    public List<string> GetPublicMethods()
+    {
+        var methods = type.GetMethods();
+        var result = new List<string>();
+        
+        foreach (var method in methods)
+        {
+            if (method.IsPublic)
+            {
+                result.Add(method.Name);
+            }
+        }
+        
+        return result;
+    }
+
+    public List<string> GetMethodParams(string methodName)
+    {
+        var method = type.GetMethod(methodName);
+        var result = new List<string>();
+        
+        if (method != null)
+        {
+            foreach (var param in method.GetParameters())
+            {
+                result.Add(param.Name);
+            }
+        }
+        
+        return result;
+    }
+
+    public List<string> GetAllFields()
+    {
+        var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        var result = new List<string>();
+        
+        foreach (var field in fields)
+        {
+            result.Add(field.Name);
+        }
+        
+        return result;
+    }
+
+    public List<string> GetProperties()
+    {
+        var properties = type.GetProperties();
+        var result = new List<string>();
+        
+        foreach (var prop in properties)
+        {
+            result.Add(prop.Name);
+        }
+        
+        return result;
+    }
+
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return type.GetCustomAttribute<T>() != null;
+    }
+}
 }
