9f79de33d2b72c33278ec3841cc1c276a523eed7
VladimirChalov <vladimircalov557@gmail.com>
2025-07-17 18:43:04 +0600

    Update UnitTest1.cs

--- task18tests/UnitTest1.cs
+++ task18tests/UnitTest1.cs
@@ -1,10 +1,43 @@
-namespace task18tests;
+using Xunit;
+using System.Threading;
+using Command;
+using task18;
 
-public class UnitTest1
+public class Task18Tests
 {
     [Fact]
-    public void Test1()
+    public void Should_Process_LongRunning_Command()
     {
+        var scheduler = new RoundRobinScheduler();
+        var server = new ServerThread(scheduler);
+        server.Start();
+        
+        var longCmd = new TestLongRunningCommand(3);
+        server.Add(longCmd);
+        
+        Thread.Sleep(100);
+        Assert.False(longCmd.IsCompleted);
+        
+        Thread.Sleep(300);
+        Assert.True(longCmd.IsCompleted);
+        
+        server.Stop();
+    }
 
+    private class TestLongRunningCommand : ICommand, ILongRunningCommand
+    {
+        private int _remainingSteps;
+        public bool IsCompleted => _remainingSteps <= 0;
+
+        public TestLongRunningCommand(int steps) => _remainingSteps = steps;
+
+        public void Execute()
+        {
+            if (_remainingSteps > 0)
+            {
+                Thread.Sleep(50);
+                _remainingSteps--;
+            }
+        }
     }
 }
