35ec7468016eafd795689cce51f99d75d693b2e8
VladimirChalov <vladimircalov557@gmail.com>
2025-07-17 19:44:52 +0600

    Update UnitTest1.cs

--- task05tests/UnitTest1.cs
+++ task05tests/UnitTest1.cs
@@ -1,10 +1,61 @@
-namespace task05tests;
+using Xunit;
+using Moq;
+using task05;
 
-public class UnitTest1
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField;
+    public int Property { get; set; }
+
+    public void Method() { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
 {
     [Fact]
-    public void Test1()
+    public void GetPublicMethods_ReturnsCorrectMethods()
     {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
 
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+        Assert.Contains("PublicField", fields);
+    }
+
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+
+        Assert.Contains("Property", properties);
+        Assert.Single(properties);
+    }
+
+    [Fact]
+    public void HasAttribute_DetectsSerializableAttribute()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        Assert.True(analyzer.HasAttribute<SerializableAttribute>());
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsFalseForMissingAttribute()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        Assert.False(analyzer.HasAttribute<SerializableAttribute>());
     }
 }
