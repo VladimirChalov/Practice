d9650b99d9151cf404805f570c99d66c6e79deea
VladimirChalov <vladimircalov557@gmail.com>
2025-07-17 18:41:37 +0600

    Update Class1.cs

--- RoundRobin/Class1.cs
+++ RoundRobin/Class1.cs
@@ -1,6 +1,20 @@
-﻿namespace RoundRobin;
+using System.Collections.Concurrent;
+using Command;
 
-public class Class1
+namespace task18
 {
+    public class RoundRobinScheduler : IScheduler
+    {
+        private readonly ConcurrentQueue<ICommand> _queue = new();
 
+        public bool HasCommand() => !_queue.IsEmpty;
+
+        public ICommand Select()
+        {
+            _queue.TryDequeue(out var command);
+            return command;
+        }
+
+        public void Add(ICommand cmd) => _queue.Enqueue(cmd);
+    }
 }
